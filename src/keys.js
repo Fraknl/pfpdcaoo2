7e0fbf911547ab290e104da755622483d5567dea
Fraknl <72723889+Fraknl@users.noreply.github.com>
2022-05-16 22:14:03 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'sistema_central'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b53c87d065b113 ',
+        password: 'df2dc6f3',
+        database: 'heroku_74f5d0c111b72ca'
     }
 }
